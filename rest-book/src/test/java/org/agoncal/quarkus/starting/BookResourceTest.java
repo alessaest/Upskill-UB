7efa20e012e2127b4969a78a111f717b27d58f56
Alessa <alessuhestaras@gmail.com>
2026-02-27 15:53:59 +0800

    fixed intial codes

---
+++ rest-book/src/test/java/org/agoncal/quarkus/starting/BookResourceTest.java
@@ -0,0 +1,20 @@
+package org.agoncal.quarkus.starting;
+
+import io.quarkus.test.junit.QuarkusTest;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.*;
+import static org.hamcrest.CoreMatchers.*;
+
+@QuarkusTest
+class BookResourceTest {
+    @Test
+    void testHelloEndpoint() {
+        given()
+          .when().get("/api/books")
+          .then()
+             .statusCode(200)
+             .body(is("Hello RESTEasy"));
+    }
+
+}
